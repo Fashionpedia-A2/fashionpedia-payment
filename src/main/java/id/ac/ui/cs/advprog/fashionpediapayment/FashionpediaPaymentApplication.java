e399dff85a3822b942ff8e63afb7f54e497c2e4f
riorio805 <sefrianojieftara@gmail.com>
2024-04-17 19:44:31 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/fashionpediapayment/FashionpediaPaymentApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.fashionpediapayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionpediaPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionpediaPaymentApplication.class, args);
+    }
+
+}
