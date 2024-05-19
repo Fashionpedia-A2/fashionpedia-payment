0827a6d9104a3863920d62f0366c24e1c566eb3e
riorio805 <sefrianojieftara@gmail.com>
2024-05-19 18:51:04 +0700

    [REFACTOR] Add Topup.toString + nominal

--- src/main/java/id/ac/ui/cs/advprog/fashionpediapayment/model/Topup.java
+++ src/main/java/id/ac/ui/cs/advprog/fashionpediapayment/model/Topup.java
@@ -15,48 +15,59 @@ public class Topup {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "topup_id")
     private String topupId;
-
     @Column(name = "buyer_id", nullable = false)
     private String buyerId;
-
     @Setter
     @Column(name = "buyer_name")
     private String buyerName;
-
     @Setter
     @Column(name = "date", nullable = false)
-    private Timestamp date;
-
+    private Timestamp date = new Timestamp(System.currentTimeMillis());
     @Column(name = "method", nullable = false)
     private String method;
-
     @Column(name = "bank_name")
     private String bankName;
-
     @Column(name = "account_number",
             columnDefinition = "char(16) NOT NULL CHECK (account_number ~ '^\\d+') ")
     private String accountNumber;
-
     @Setter
     @Column(name = "photo_proof")
     private String photoProof;
-
     @Setter
     @Column(name = "approval", nullable = false)
-    private String approval;
+    private String approval = "PENDING";
+    @Column(name = "nominal", nullable = false)
+    private long nominal;
 
     // Constructors
     public Topup() {
     }
 
-    public Topup(String buyerId, String method, String bankName, String accountNumber) {
+    public Topup(String buyerId, String method, String bankName, String accountNumber, long nominal) {
         this.buyerId = buyerId;
         this.method = method;
         this.bankName = bankName;
         this.accountNumber = accountNumber;
+        this.nominal = nominal;
     }
-    public Topup(String buyerId, String method, String bankName, String accountNumber, String photoProof) {
-        this(buyerId, method, bankName, accountNumber);
+    public Topup(String buyerId, String method, String bankName, String accountNumber, long nominal, String photoProof) {
+        this(buyerId, method, bankName, accountNumber, nominal);
         this.photoProof = photoProof;
+    }
+
+    @Override
+    public String toString() {
+        return "Topup {\n\t" +
+                "topupId = " + topupId + ",\n\t" +
+                "buyerId = " + buyerId + ",\n\t" +
+                "buyerName = " + buyerName + ",\n\t" +
+                "date = " + date.toString() + ",\n\t" +
+                "method = " + method + ",\n\t" +
+                "bankName = " + bankName + ",\n\t" +
+                "accountNumber = " + accountNumber + ",\n\t" +
+                "photoProof = " + photoProof + ",\n\t" +
+                "approval = " + approval + ",\n\t" +
+                "nominal = " + nominal + "\n" +
+            "}";
     }
 }
