3c016642df54a476b12c0d7cd7d1a33ea1bb0c8e
riorio805 <sefrianojieftara@gmail.com>
2024-05-26 23:07:54 +0700

    More service tests

--- src/main/java/id/ac/ui/cs/advprog/fashionpediapayment/exceptions/TopupException.java
+++ src/main/java/id/ac/ui/cs/advprog/fashionpediapayment/exceptions/TopupException.java
@@ -3,13 +3,15 @@ package id.ac.ui.cs.advprog.fashionpediapayment.exceptions;
 
 public class TopupException extends PaymentServiceException {
     public enum TopupExceptionType {
-        TopupIdDoesNotExist,
-        TopupAlreadyCancelled,
-        TopupCannotBeCancelled,
+        WrongOrEmptyFields, // 2800
+        TopupIdDoesNotExist, // 2820
+        TopupAlreadyCancelled, // 2821
+        TopupCannotBeCancelled, // 2822
     }
 
     private static int typeToErrorCode(TopupExceptionType type){
         return switch (type) {
+            case WrongOrEmptyFields-> 2800;
             case TopupIdDoesNotExist -> 2820;
             case TopupAlreadyCancelled -> 2821;
             case TopupCannotBeCancelled -> 2822;
